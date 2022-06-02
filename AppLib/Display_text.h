b5b65fa8d02b6a850b1487f8e566ae014e756f19
twezowic <01169258@pw.edu.pl>
2022-06-02 15:04:09 +0200

    Added Warsaw' Metro.

--- AppLib/Display_text.h
+++ AppLib/Display_text.h
@@ -8,7 +8,7 @@ class Display_text
 protected:
 	int width, height;
 public:
-	Display_text() { this->width = 25; this->height = 25; }
+	Display_text() { this->width = 30; this->height = 30; }
 	Display_text(int width, int height) { this->width = width; this->height = height; }
 
 	virtual void create_map(MetroApp metroapp);
